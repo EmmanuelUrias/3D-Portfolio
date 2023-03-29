4f22b10a680f3dca60bf4948e1ade508938fe5cc
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-28 22:58:36 -0700

    finished hero section, imported 3d model

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false)
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+    <nav className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-6xl mx-auto'>
         <Link to='/' className='flex items-center gap-2' onClick={() => { setActive(''), window.scrollTo(0, 0)}}>
-          <img src={logo} alt='logo' className='w-[100px] h-[70px] object-contain' />
+          <img src={logo} alt='logo' className='w-[120px] h-[80px] object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>Emmanuel &nbsp;<span className='sm:block hidden'>Urias</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
