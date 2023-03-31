dc17d3346d18d54446ef049e55a38713c96769bb
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-30 20:21:40 -0700

    added comments and changed some data

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,36 +9,37 @@ const Computers = ({ isMobile }) => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black'/>
-      <pointLight intensity={1}/>
+      <pointLight intensity={1}/> {/* Line above, under, and this line are all lighting */}
       <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024}/>
-      <primitive object={computer.scene} scale={isMobile ? 0.5 : 0.75} position={isMobile ? [-3, -2.9, -1.5] : [0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]}/>
+      <primitive object={computer.scene} scale={isMobile ? 0.5 : 0.75} position={isMobile ? [-3, -2.9, -1.5] : [0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]}/> {/* Computer Model */}
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false)
+
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500px)')
+    const mediaQuery = window.matchMedia('(max-width: 500px)') // Gets screen width
 
-    setIsMobile(mediaQuery.matches)
+    setIsMobile(mediaQuery.matches) // are we on the device that is smaller than the width of 500px
 
-    const handleMediaQueryChange = (e) => {
+    const handleMediaQueryChange = (e) => { // If the matches changes this will set isMobile to the new match
       setIsMobile(e.matches)
     }
 
-    mediaQuery.addEventListener('change', handleMediaQueryChange)
+    mediaQuery.addEventListener('change', handleMediaQueryChange) // calls the handleMediaQueryChange when the event listener listens to a change
 
     return () => {
-      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+      mediaQuery.removeEventListener('change', handleMediaQueryChange) // we have to close the eventListener in a useEffect function
     }
   }, [])
 
   return (  
-    <Canvas frameLoop="demand" shadows camera={{position: [20, 3, 5], fov: 25}} gl={{ preserveDrawingBuffer: true }}>
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
-        <Computers isMobile={isMobile}/>
+    <Canvas frameLoop="demand" shadows camera={{ position: [20, 3, 5], fov: 25}} gl={{ preserveDrawingBuffer: true }}> {/* Canvas/background to render the 3d model */}
+      <Suspense fallback={<CanvasLoader />}> {/* Loading screen */}
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/> {/* When rotating the 3d model this keeps it on a horizontal plane */}
+        <Computers isMobile={isMobile}/> 
       </Suspense>
       <Preload all />
     </Canvas>
