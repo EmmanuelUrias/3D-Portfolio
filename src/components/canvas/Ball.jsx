421ade9641b2620df5ed072ef5ef2ba224e29e57
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-31 19:11:43 -0700

    made technology 3d models

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { Suspense, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
+import CanvasLoader from '../Loader'
+import ParticleSystem from '../../utils/ParticleSystem'
 
-const Ball = () => {
+const Ball = (props) => {
+  const [ decal ] = useTexture([props.imgUrl])
+  const [ clicked, setClicked ] = useState(false)
+
+  const handleClick = () => {
+    setClicked(true)
+  }
+
+  
+
+
+  if(clicked) {
+    return (
+      <ParticleSystem position={[0, 0, 1]} />
+    )
+  }
+  
+
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2} onClick={handleClick}>
+      <ambientLight intensity={0.25}/>
+      <directionalLight position={[0, 0, 0.05]}/>
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
+        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} flatShading map={decal}></Decal>
+      </mesh>
+    </Float>
   )
 }
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas frameLoop='demand' gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false}/>
+        <Ball imgUrl={icon}/>
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default BallCanvas
